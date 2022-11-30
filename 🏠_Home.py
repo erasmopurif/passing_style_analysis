02aba181486e3936e6e3fd5768108d6722fd7c60
Erasmo Purificato <erasmopurif@gmai.com>
2022-11-30 04:26:26 +0100

    Spell correction in description

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -50,13 +50,13 @@ st.title("Passing Style Analyser - Serie A 2021/22")
 # App description
 st.markdown(
     """
-    With this application, you can analyze the passing style of each Serie A 2021/22 team.
+    With this application, you can analyse the passing style of each Serie A 2021/22 team.
 
     The two pages available in the sidebar are described below.
 
     ## Full Pitch & Field Tilt Networks
     
-    After selected a **single matchday** or a **range of matchdays**, it is possible to analyze:
+    After selected a **single matchday** or a **range of matchdays**, it is possible to analyse:
     * a single team,
     * two teams, or
     * all the 20 Serie A teams,
@@ -66,7 +66,7 @@ st.markdown(
 
     ## Match Status Networks
 
-    After selected a **single matchday** or a **range of matchdays**, and a **team**, it is possible to analyze the full-pitch passing network for each **match status** (i.e. winning, drawing, losing) and view their barycenter in the "passing" scenario.
+    After selected a **single matchday** or a **range of matchdays**, and a **team**, it is possible to analyse the full-pitch passing network for each **match status** (i.e. winning, drawing, losing) and view their barycenter in the "passing" scenario.
 
     Additionally, the user can decide whether or not to keep the goalkeeper in the barycenter computation, and select the number of minimum passes to be displayed in the passing networks (Note that in this case, for multiple matchdays, this value is **NOT** multiplied by the number of selected matchdays).
 
